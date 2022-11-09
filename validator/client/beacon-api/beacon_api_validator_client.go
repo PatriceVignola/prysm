f47d27812917077a4b02fd167374fbc53fb7074d
Patrice Vignola <vignola.patrice@gmail.com>
2022-11-09 00:58:57 -0800

    WIP

--- validator/client/beacon-api/beacon_api_validator_client.go
+++ validator/client/beacon-api/beacon_api_validator_client.go
@@ -4,8 +4,12 @@
 package beacon_api
 
 import (
+	"bytes"
 	"context"
+	"sort"
+	"strings"
 
+	"encoding/hex"
 	"encoding/json"
 	"net/http"
 	"strconv"
@@ -13,6 +17,7 @@ import (
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/pkg/errors"
+	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
 	ethpb "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
 	iface "github.com/prysmaticlabs/prysm/v3/validator/client/iface"
 )
@@ -22,9 +27,22 @@ type beaconApiValidatorClient struct {
 	httpClient http.Client
 }
 
-func (*beaconApiValidatorClient) GetDuties(_ context.Context, _ *ethpb.DutiesRequest) (*ethpb.DutiesResponse, error) {
-	// TODO: Implement me
-	panic("beaconApiValidatorClient.GetDuties is not implemented")
+func (c *beaconApiValidatorClient) GetDuties(_ context.Context, in *ethpb.DutiesRequest) (*ethpb.DutiesResponse, error) {
+	currentEpochDuties, err := c.getDuties(uint64(in.Epoch), in.PublicKeys)
+	if err != nil {
+		return nil, err
+	}
+
+	nextEpochDuties, err := c.getDuties(uint64(in.Epoch)+1, in.PublicKeys)
+	if err != nil {
+		return nil, err
+	}
+
+	dutiesResponse := &ethpb.DutiesResponse{}
+	dutiesResponse.Duties = currentEpochDuties
+	dutiesResponse.CurrentEpochDuties = currentEpochDuties
+	dutiesResponse.NextEpochDuties = nextEpochDuties
+	return dutiesResponse, nil
 }
 
 func (*beaconApiValidatorClient) CheckDoppelGanger(_ context.Context, _ *ethpb.DoppelGangerRequest) (*ethpb.DoppelGangerResponse, error) {
@@ -67,9 +85,8 @@ func (*beaconApiValidatorClient) GetSyncSubcommitteeIndex(_ context.Context, _ *
 	panic("beaconApiValidatorClient.GetSyncSubcommitteeIndex is not implemented")
 }
 
-func (*beaconApiValidatorClient) MultipleValidatorStatus(_ context.Context, _ *ethpb.MultipleValidatorStatusRequest) (*ethpb.MultipleValidatorStatusResponse, error) {
-	// TODO: Implement me
-	panic("beaconApiValidatorClient.MultipleValidatorStatus is not implemented")
+func (c *beaconApiValidatorClient) MultipleValidatorStatus(_ context.Context, in *ethpb.MultipleValidatorStatusRequest) (*ethpb.MultipleValidatorStatusResponse, error) {
+	return c.getMultipleValidatorStatus(in.PublicKeys, in.Indices)
 }
 
 func (*beaconApiValidatorClient) PrepareBeaconProposer(_ context.Context, _ *ethpb.PrepareBeaconProposerRequest) (*empty.Empty, error) {
@@ -137,9 +154,8 @@ func (*beaconApiValidatorClient) ValidatorIndex(_ context.Context, _ *ethpb.Vali
 	panic("beaconApiValidatorClient.ValidatorIndex is not implemented")
 }
 
-func (*beaconApiValidatorClient) ValidatorStatus(_ context.Context, _ *ethpb.ValidatorStatusRequest) (*ethpb.ValidatorStatusResponse, error) {
-	// TODO: Implement me
-	panic("beaconApiValidatorClient.ValidatorStatus is not implemented")
+func (c *beaconApiValidatorClient) ValidatorStatus(_ context.Context, in *ethpb.ValidatorStatusRequest) (*ethpb.ValidatorStatusResponse, error) {
+	return c.getValidatorStatus(in.PublicKey)
 }
 
 func (*beaconApiValidatorClient) WaitForActivation(_ context.Context, _ *ethpb.ValidatorActivationRequest) (ethpb.BeaconNodeValidator_WaitForActivationClient, error) {
@@ -193,4 +209,462 @@ func NewBeaconApiValidatorClient() iface.ValidatorClient {
 	return &beaconApiValidatorClient{"http://127.0.0.1:3500", http.Client{
 		Timeout: 120 * time.Second,
 	}}
+}
+
+func (c *beaconApiValidatorClient) getValidatorStatus(pubkey []byte) (*ethpb.ValidatorStatusResponse, error) {
+	resp, err := c.httpClient.Get(c.address + "/eth/v1/beacon/states/head/validators/0x" + hex.EncodeToString(pubkey))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		errorJson := ErrorResponseJson{}
+		err = json.NewDecoder(resp.Body).Decode(&errorJson)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.Errorf("error %d: %s", errorJson.Code, errorJson.Message)
+	}
+
+	responseJson := &StateValidatorResponseJson{}
+	err = json.NewDecoder(resp.Body).Decode(&responseJson)
+	if err != nil {
+		return nil, err
+	}
+
+	activationQueue, err := c.getActivationQueue()
+	if err != nil {
+		return nil, err
+	}
+
+	statusResponse, err := parseValidatorStatusResponse(responseJson.Data, activationQueue)
+	if err != nil {
+		return nil, err
+	}
+
+	return statusResponse, nil
+}
+
+func (c *beaconApiValidatorClient) getMultipleValidatorStatus(pubkeys [][]byte, indices []int64) (*ethpb.MultipleValidatorStatusResponse, error) {
+	query := c.address + "/eth/v1/beacon/states/head/validators"
+
+	if len(indices) > 0 || len(pubkeys) > 0 {
+		var queryArgs strings.Builder
+		if _, err := queryArgs.WriteString("?id="); err != nil {
+			return nil, err
+		}
+
+		// Append the indices to the query string
+		for i, validatorIndex := range indices {
+			if _, err := queryArgs.WriteString(strconv.FormatInt(validatorIndex, 10)); err != nil {
+				return nil, err
+			}
+
+			// Don't add a comma if it's the last element
+			if i < len(indices)-1 || len(pubkeys) != 0 {
+				if _, err := queryArgs.WriteString(","); err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		// Append the public keys to the query string
+		for i, pubkey := range pubkeys {
+			if _, err := queryArgs.WriteString("0x" + hex.EncodeToString(pubkey)); err != nil {
+				return nil, err
+			}
+
+			// Don't add a comma if it's the last element
+			if i < len(pubkeys)-1 {
+				if _, err := queryArgs.WriteString(","); err != nil {
+					return nil, err
+				}
+			}
+			break
+		}
+
+		query += queryArgs.String()
+	}
+
+	resp, err := c.httpClient.Get(query)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		errorJson := ErrorResponseJson{}
+		err = json.NewDecoder(resp.Body).Decode(&errorJson)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.Errorf("error %d: %s", errorJson.Code, errorJson.Message)
+	}
+
+	responseJson := &StateValidatorsResponseJson{}
+	err = json.NewDecoder(resp.Body).Decode(&responseJson)
+	if err != nil {
+		return nil, err
+	}
+
+	activationQueue, err := c.getActivationQueue()
+	if err != nil {
+		return nil, err
+	}
+
+	response := &ethpb.MultipleValidatorStatusResponse{}
+	for _, responseData := range responseJson.Data {
+		validatorIndex, err := strconv.ParseUint(responseData.Index, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Indices = append(response.Indices, types.ValidatorIndex(validatorIndex))
+		response.PublicKeys = append(response.PublicKeys, []byte(responseData.Validator.PublicKey))
+
+		statusResponse, err := parseValidatorStatusResponse(responseData, activationQueue)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Statuses = append(response.Statuses, statusResponse)
+	}
+
+	return response, nil
+}
+
+// Returns the index of the next validator to be activated, or nil if the activation queue is empty
+func (c *beaconApiValidatorClient) getActivationQueue() (*StateValidatorsResponseJson, error) {
+	resp, err := c.httpClient.Get(c.address + "/eth/v1/beacon/states/head/validators?status=pending_queued")
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		errorJson := ErrorResponseJson{}
+		err = json.NewDecoder(resp.Body).Decode(&errorJson)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.Errorf("error %d: %s", errorJson.Code, errorJson.Message)
+	}
+
+	responseJson := &StateValidatorsResponseJson{}
+	err = json.NewDecoder(resp.Body).Decode(&responseJson)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(responseJson.Data) == 0 {
+		return nil, nil
+	}
+
+	sort.Slice(responseJson.Data, func(i, j int) bool {
+		myActivationEligibilityEpoch, err := strconv.ParseUint(responseJson.Data[i].Validator.ActivationEligibilityEpoch, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		otherActivationEligibilityEpoch, err := strconv.ParseUint(responseJson.Data[j].Validator.ActivationEligibilityEpoch, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		if myActivationEligibilityEpoch == otherActivationEligibilityEpoch {
+			myIndex, err := strconv.ParseUint(responseJson.Data[i].Index, 10, 64)
+			if err != nil {
+				return false
+			}
+
+			otherIndex, err := strconv.ParseUint(responseJson.Data[j].Index, 10, 64)
+			if err != nil {
+				return false
+			}
+
+			return myIndex < otherIndex
+		}
+
+		return myActivationEligibilityEpoch < otherActivationEligibilityEpoch
+	})
+
+	return responseJson, nil
+}
+
+func (c *beaconApiValidatorClient) getAttesterDuties(epoch uint64, validatorIndices []string) (*AttesterDutiesResponseJson, error) {
+	jsonIndices, err := json.Marshal(validatorIndices)
+	if err != nil {
+		return nil, err
+	}
+
+	query := c.address + "/eth/v1/validator/duties/attester/" + strconv.FormatUint(epoch, 10)
+	resp, err := c.httpClient.Post(query, "application/json", bytes.NewBuffer(jsonIndices))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		errorJson := ErrorResponseJson{}
+		err = json.NewDecoder(resp.Body).Decode(&errorJson)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.Errorf("error %d: %s", errorJson.Code, errorJson.Message)
+	}
+
+	dutiesJson := &AttesterDutiesResponseJson{}
+	err = json.NewDecoder(resp.Body).Decode(&dutiesJson)
+	if err != nil {
+		return nil, err
+	}
+
+	return dutiesJson, nil
+}
+
+func (c *beaconApiValidatorClient) getProposerDuties(epoch uint64) (*ProposerDutiesResponseJson, error) {
+	resp, err := c.httpClient.Get(c.address + "/eth/v1/validator/duties/proposer/" + strconv.FormatUint(epoch, 10))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		errorJson := ErrorResponseJson{}
+		err = json.NewDecoder(resp.Body).Decode(&errorJson)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.Errorf("error %d: %s", errorJson.Code, errorJson.Message)
+	}
+
+	dutiesJson := &ProposerDutiesResponseJson{}
+	err = json.NewDecoder(resp.Body).Decode(&dutiesJson)
+	if err != nil {
+		return nil, err
+	}
+
+	return dutiesJson, nil
+}
+
+func (c *beaconApiValidatorClient) getSyncDuties(epoch uint64, validatorIndices []string) (*SyncCommitteeDutiesResponseJson, error) {
+	jsonIndices, err := json.Marshal(validatorIndices)
+	if err != nil {
+		return nil, err
+	}
+
+	query := c.address + "/eth/v1/validator/duties/sync/" + strconv.FormatUint(epoch, 10)
+	resp, err := c.httpClient.Post(query, "application/json", bytes.NewBuffer(jsonIndices))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		errorJson := ErrorResponseJson{}
+		err = json.NewDecoder(resp.Body).Decode(&errorJson)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.Errorf("error %d: %s", errorJson.Code, errorJson.Message)
+	}
+
+	dutiesJson := &SyncCommitteeDutiesResponseJson{}
+	err = json.NewDecoder(resp.Body).Decode(&dutiesJson)
+	if err != nil {
+		return nil, err
+	}
+
+	return dutiesJson, nil
+}
+
+func (c *beaconApiValidatorClient) getDuties(epoch uint64, pubkeys [][]byte) ([]*ethpb.DutiesResponse_Duty, error) {
+	multipleValidatorStatus, err := c.getMultipleValidatorStatus(pubkeys, []int64{})
+	if err != nil {
+		return nil, err
+	}
+
+	validatorIndices := multipleValidatorStatus.Indices
+	indicesMapping := make(map[uint64]*ethpb.DutiesResponse_Duty)
+	validatorStringIndices := make([]string, 0, len(validatorIndices))
+	for _, validatorIndex := range validatorIndices {
+		validatorStringIndices = append(validatorStringIndices, strconv.FormatUint(uint64(validatorIndex), 10))
+		indicesMapping[uint64(validatorIndex)] = &ethpb.DutiesResponse_Duty{}
+	}
+
+	attesterDutiesJson, err := c.getAttesterDuties(epoch, validatorStringIndices)
+	if err != nil {
+		return nil, err
+	}
+
+	proposerDutiesJson, err := c.getProposerDuties(epoch)
+	if err != nil {
+		return nil, err
+	}
+
+	syncDutiesJson, err := c.getSyncDuties(epoch, validatorStringIndices)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the attester duties
+	for _, attesterDutyData := range attesterDutiesJson.Data {
+		validatorIndex, err := strconv.ParseUint(attesterDutyData.ValidatorIndex, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		attesterSlot, err := strconv.ParseUint(attesterDutyData.Slot, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		if dutyResponse, exists := indicesMapping[validatorIndex]; exists {
+			dutyResponse.PublicKey = []byte(attesterDutyData.Pubkey)
+			dutyResponse.ValidatorIndex = types.ValidatorIndex(validatorIndex)
+			dutyResponse.AttesterSlot = types.Slot(attesterSlot)
+		}
+	}
+
+	// Get the proposer duties
+	for _, proposerDutyData := range proposerDutiesJson.Data {
+		validatorIndex, err := strconv.ParseUint(proposerDutyData.ValidatorIndex, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		proposerSlot, err := strconv.ParseUint(proposerDutyData.ValidatorIndex, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		if dutyResponse, exists := indicesMapping[validatorIndex]; exists {
+			dutyResponse.ProposerSlots = append(dutyResponse.ProposerSlots, types.Slot(proposerSlot))
+		}
+	}
+
+	// Get the sync duties
+	for _, dutyData := range syncDutiesJson.Data {
+		validatorIndex, err := strconv.ParseUint(dutyData.ValidatorIndex, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		if dutyResponse, exists := indicesMapping[validatorIndex]; exists {
+			dutyResponse.PublicKey = []byte(dutyData.Pubkey)
+			dutyResponse.IsSyncCommittee = true
+
+			for _, committeeIndex := range dutyData.ValidatorSyncCommitteeIndices {
+				uintCommitteeIndex, err := strconv.ParseUint(committeeIndex, 10, 64)
+				if err != nil {
+					return nil, err
+				}
+
+				dutyResponse.Committee = append(dutyResponse.Committee, types.ValidatorIndex(uintCommitteeIndex))
+			}
+		}
+	}
+
+	// Finally, fill the return array with the elements from the map
+	duties := make([]*ethpb.DutiesResponse_Duty, 0, len(validatorIndices))
+	for pubkeyIndex, validatorIndex := range validatorIndices {
+		validatorStatus, err := c.getValidatorStatus(pubkeys[pubkeyIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		dutyResponse := indicesMapping[uint64(validatorIndex)]
+		dutyResponse.ValidatorIndex = types.ValidatorIndex(validatorIndex)
+		dutyResponse.PublicKey = pubkeys[pubkeyIndex]
+		dutyResponse.Status = validatorStatus.Status
+		duties = append(duties, dutyResponse)
+	}
+
+	return duties, nil
+}
+
+func parseValidatorStatusResponse(responseData *ValidatorContainerJson, activationQueue *StateValidatorsResponseJson) (*ethpb.ValidatorStatusResponse, error) {
+	activationEpoch, err := strconv.ParseUint(responseData.Validator.ActivationEpoch, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	statusResponse := &ethpb.ValidatorStatusResponse{}
+	statusResponse.ActivationEpoch = types.Epoch(activationEpoch)
+	statusResponse.Status = convertStringStatusToEnum(responseData.Status)
+
+	isPending := statusResponse.Status == ethpb.ValidatorStatus_DEPOSITED ||
+		statusResponse.Status == ethpb.ValidatorStatus_PARTIALLY_DEPOSITED ||
+		statusResponse.Status == ethpb.ValidatorStatus_PENDING
+
+	if isPending {
+		// Count the number of validators that are ahead of us in the activation queue
+		var indexInActivationQueue uint64
+		for i, pendingValidatorData := range activationQueue.Data {
+			if pendingValidatorData.Index == responseData.Index {
+				indexInActivationQueue = uint64(i)
+				break
+			}
+		}
+
+		statusResponse.PositionInActivationQueue = indexInActivationQueue + 1
+	}
+
+	return statusResponse, nil
+}
+
+func convertStringStatusToEnum(jsonStatus string) ethpb.ValidatorStatus {
+	switch jsonStatus {
+	case "pending_initialized":
+		return ethpb.ValidatorStatus_PARTIALLY_DEPOSITED
+	case "pending_queued":
+		return ethpb.ValidatorStatus_DEPOSITED
+	case "active_ongoing":
+		return ethpb.ValidatorStatus_ACTIVE
+	case "active_exiting":
+		return ethpb.ValidatorStatus_EXITING
+	case "active_slashed":
+		return ethpb.ValidatorStatus_SLASHING
+	case "exited_unslashed":
+		return ethpb.ValidatorStatus_EXITED
+	case "exited_slashed":
+		return ethpb.ValidatorStatus_EXITED
+	case "withdrawal_possible":
+		return ethpb.ValidatorStatus_EXITED
+	case "withdrawal_done":
+		return ethpb.ValidatorStatus_EXITED
+	default:
+		return ethpb.ValidatorStatus_UNKNOWN_STATUS
+	}
 }
--- validator/client/beacon-api/json_types.go
+++ validator/client/beacon-api/json_types.go
@@ -15,3 +15,64 @@ type GenesisResponseJson struct {
 		GenesisForkVersion    string `json:"genesis_fork_version" hex:"true"`
 	} `json:"data"`
 }
+
+type AttesterDutiesResponseJson struct {
+	DependentRoot string `json:"dependent_root" hex:"true"`
+	Data          []struct {
+		Pubkey                  string `json:"pubkey" hex:"true"`
+		ValidatorIndex          string `json:"validator_index"`
+		CommitteeIndex          string `json:"committee_index"`
+		CommitteeLength         string `json:"committee_length"`
+		CommitteesAtSlot        string `json:"committees_at_slot"`
+		ValidatorCommitteeIndex string `json:"validator_committee_index"`
+		Slot                    string `json:"slot"`
+	} `json:"data"`
+	ExecutionOptimistic bool `json:"execution_optimistic"`
+}
+
+type ProposerDutiesResponseJson struct {
+	DependentRoot string `json:"dependent_root" hex:"true"`
+	Data          []struct {
+		Pubkey         string `json:"pubkey" hex:"true"`
+		ValidatorIndex string `json:"validator_index"`
+		Slot           string `json:"slot"`
+	} `json:"data"`
+	ExecutionOptimistic bool `json:"execution_optimistic"`
+}
+
+type SyncCommitteeDutiesResponseJson struct {
+	Data []struct {
+		Pubkey                        string   `json:"pubkey" hex:"true"`
+		ValidatorIndex                string   `json:"validator_index"`
+		ValidatorSyncCommitteeIndices []string `json:"validator_sync_committee_indices"`
+	} `json:"data"`
+	ExecutionOptimistic bool `json:"execution_optimistic"`
+}
+
+type ValidatorJson struct {
+	PublicKey                  string `json:"pubkey" hex:"true"`
+	WithdrawalCredentials      string `json:"withdrawal_credentials" hex:"true"`
+	EffectiveBalance           string `json:"effective_balance"`
+	Slashed                    bool   `json:"slashed"`
+	ActivationEligibilityEpoch string `json:"activation_eligibility_epoch"`
+	ActivationEpoch            string `json:"activation_epoch"`
+	ExitEpoch                  string `json:"exit_epoch"`
+	WithdrawableEpoch          string `json:"withdrawable_epoch"`
+}
+
+type ValidatorContainerJson struct {
+	Index     string         `json:"index"`
+	Balance   string         `json:"balance"`
+	Status    string         `json:"status" enum:"true"`
+	Validator *ValidatorJson `json:"validator"`
+}
+
+type StateValidatorsResponseJson struct {
+	Data                []*ValidatorContainerJson `json:"data"`
+	ExecutionOptimistic bool                      `json:"execution_optimistic"`
+}
+
+type StateValidatorResponseJson struct {
+	Data                *ValidatorContainerJson `json:"data"`
+	ExecutionOptimistic bool                    `json:"execution_optimistic"`
+}
