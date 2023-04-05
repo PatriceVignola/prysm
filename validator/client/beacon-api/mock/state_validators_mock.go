1588c6e20443c88413f18d6a5ac4ebe5432c312e
Patrice Vignola <vignola.patrice@gmail.com>
2023-04-04 23:34:49 -0700

    Add missing mocks

--- validator/client/beacon-api/mock/state_validators_mock.go
+++ validator/client/beacon-api/mock/state_validators_mock.go
@@ -10,6 +10,7 @@ import (
 
 	gomock "github.com/golang/mock/gomock"
 	apimiddleware "github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/apimiddleware"
+	primitives "github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 )
 
 // MockstateValidatorsProvider is a mock of stateValidatorsProvider interface.
@@ -48,4 +49,19 @@ func (m *MockstateValidatorsProvider) GetStateValidators(arg0 context.Context, a
 func (mr *MockstateValidatorsProviderMockRecorder) GetStateValidators(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetStateValidators", reflect.TypeOf((*MockstateValidatorsProvider)(nil).GetStateValidators), arg0, arg1, arg2, arg3)
+}
+
+// GetStateValidatorsForSlot mocks base method.
+func (m *MockstateValidatorsProvider) GetStateValidatorsForSlot(arg0 context.Context, arg1 primitives.Slot, arg2 []string, arg3 []int64, arg4 []string) (*apimiddleware.StateValidatorsResponseJson, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetStateValidatorsForSlot", arg0, arg1, arg2, arg3, arg4)
+	ret0, _ := ret[0].(*apimiddleware.StateValidatorsResponseJson)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetStateValidatorsForSlot indicates an expected call of GetStateValidatorsForSlot.
+func (mr *MockstateValidatorsProviderMockRecorder) GetStateValidatorsForSlot(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetStateValidatorsForSlot", reflect.TypeOf((*MockstateValidatorsProvider)(nil).GetStateValidatorsForSlot), arg0, arg1, arg2, arg3, arg4)
 }
